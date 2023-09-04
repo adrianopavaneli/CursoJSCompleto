50cb42534d06cfcf3dbd586db910f86d4998cf14
Adriano Pavaneli <adriano.pavaneli.dev@gmail.com>
2023-09-03 22:50:52 -0300

    logica

---
+++ logica/operadores comparacao/index.js
@@ -0,0 +1,11 @@
+/*
+Operadores de comparação
+> maior que
+>= maior ou igual que
+< menor que
+<= menor ou igual que
+== igualdade (valor) ************* nao usar
+=== igualdade estrito(valor e tipo)
+!= diferente  ********* nao usar
+!== diferente estrito(valor e tipo)
+*/
---
+++ logica/operadores logicos/index.js
@@ -0,0 +1,6 @@
+/*
+Operadores Logico
+&& -> AND -> E -> Todas as expressoes precisam ser verdadeiras
+|| -> OR -> OU - > so mente uma das expressoes precisa ser verdadeiras
+! -> NOT -> NÃO -> tem que ser negativa para retornar true - inverte valores
+*/
