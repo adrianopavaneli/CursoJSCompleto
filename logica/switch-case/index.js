1d78876aca0f3958d8334990f743b8f50558bbb2
Adriano Pavaneli <adriano.pavaneli.dev@gmail.com>
2023-09-07 23:27:03 -0300

    switch case

---
+++ logica/switch-case/index.js
@@ -0,0 +1,64 @@
+const data = new Date('1987-04-21 00:00:00');
+let diaSemana = data.getDay();
+let diaSemanaTexto = getDiaSemanaTexto(diaSemana);
+
+// switch (diaSemana) {
+    
+//     case 0:
+//         diaSemanaTexto = 'Domingo';
+//         break;
+//     case 1:
+//         diaSemanaTexto = 'Segunda';
+//         break;
+//     case 2:
+//         diaSemanaTexto = 'Terça';
+//         break;
+//     case 3:
+//         diaSemanaTexto = 'Quarta-feira';
+//         break;
+//     case 4:
+//         diaSemanaTexto = 'Quinta-feira';
+//         break;
+//     case 5:
+//         diaSemanaTexto = 'Sexta-feira';
+//         break;
+//     case 6:
+//         diaSemanaTexto = 'Sábado';
+//         break;
+//     default:
+//         diaSemanaTexto = '';
+//         break;
+// }
+
+function getDiaSemanaTexto (diaSemana) {
+    let diaSemanaTexto;
+    switch (diaSemana){
+    case 0:
+        diaSemanaTexto = 'Domingo';
+        return diaSemanaTexto;
+    case 1:
+        diaSemanaTexto = 'Segunda';
+        return diaSemanaTexto;
+    case 2:
+        diaSemanaTexto = 'Terça';
+        return diaSemanaTexto;
+    case 3:
+        diaSemanaTexto = 'Quarta-feira';
+        return diaSemanaTexto;
+    case 4:
+        diaSemanaTexto = 'Quinta-feira';
+        return diaSemanaTexto;
+    case 5:
+        diaSemanaTexto = 'Sexta-feira';
+        return diaSemanaTexto;
+    case 6:
+        diaSemanaTexto = 'Sábado';
+        return diaSemanaTexto;
+    default:
+        diaSemanaTexto = '';
+        return diaSemanaTexto;
+    }
+}        
+
+
+console.log(diaSemana ,diaSemanaTexto);
